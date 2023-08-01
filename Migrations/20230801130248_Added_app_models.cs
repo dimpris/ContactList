8b6d64addfb8d4d6238b025050a37c553df76a40
Dmytro Prysiazhnyi <dimpris@gmail.com>
2023-08-01 16:49:50 +0300

    Added migrations

---
+++ Migrations/20230801130248_Added_app_models.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ContactList.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_app_models : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactListRole",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactListRole", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactListUser",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fullname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    VerifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactListUser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContactListUser_ContactListRole_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "ContactListRole",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contacts_ContactListUser_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "ContactListUser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Phones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    ContactId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Phones_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ContactListRole",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "administrator" },
+                    { 2, "user" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ContactListUser",
+                columns: new[] { "Id", "CreatedAt", "Email", "Fullname", "Login", "PasswordHash", "RoleId", "VerifiedAt" },
+                values: new object[] { 1, new DateTime(2023, 8, 1, 16, 2, 47, 286, DateTimeKind.Local).AddTicks(7325), "john.doe@example.com", "John Doe", "john_doe", "john_doe", 2, new DateTime(2023, 8, 1, 13, 2, 47, 286, DateTimeKind.Utc).AddTicks(7405) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactListUser_RoleId",
+                table: "ContactListUser",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_OwnerId",
+                table: "Contacts",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Phones_ContactId",
+                table: "Phones",
+                column: "ContactId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Phones");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "ContactListUser");
+
+            migrationBuilder.DropTable(
+                name: "ContactListRole");
+        }
+    }
+}
