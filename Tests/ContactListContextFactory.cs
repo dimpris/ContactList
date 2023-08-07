29e8f62a4c380ebc633ef2a1b0be9951fb937b1e
Dmytro Prysiazhnyi <dimpris@gmail.com>
2023-08-07 14:15:44 +0300

    Added unit tests

---
+++ Tests/ContactListContextFactory.cs
@@ -0,0 +1,74 @@
+﻿using ContactList.DataContexts;
+using ContactList.DataServices;
+using ContactList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests
+{
+    public class ContactListContextFactory
+    {
+        public static int UserId = 1;
+
+        public static int AdminRoleId = 1;
+        public static int UserRoleId = 2;
+
+        public static int ContactForEditId = 2;
+        public static int ContactForDeleteId = 3;
+        public static ContactListAppContext Create()
+        {
+            var options = new DbContextOptionsBuilder<ContactListAppContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new ContactListAppContext(options);
+            context.Database.EnsureCreated();
+
+            Contact contactForEdit = new Contact();
+            contactForEdit.Id = ContactForEditId;
+            contactForEdit.Name = "Contact For Edit";
+            contactForEdit.Email = "test@example.com";
+            contactForEdit.OwnerId = UserId;
+            contactForEdit.Phones = new List<Phone>()
+            {
+                new Phone()
+                {
+                    Type = PhoneType.Mobile,
+                    Number = "(123) 456-7890"
+                },
+                new Phone()
+                {
+                    Type = PhoneType.Work,
+                    Number = "(123) 456-7890"
+                }
+            };
+            Contact contactForDelete = new Contact();
+            contactForDelete.Id = ContactForDeleteId;
+            contactForDelete.Name = "Contact For Delete";
+            contactForDelete.Email = "test@example.com";
+            contactForDelete.OwnerId = UserId;
+            contactForDelete.Phones = new List<Phone>()
+            {
+                new Phone()
+                {
+                    Type = PhoneType.Mobile,
+                    Number = "(123) 456-7890"
+                },
+                new Phone()
+                {
+                    Type = PhoneType.Work,
+                    Number = "(123) 456-7890"
+                }
+            };
+            context.Contacts.Add(contactForEdit);
+            context.Contacts.Add(contactForDelete);
+
+            context.SaveChanges();
+            return context;
+        }
+
+        public static void Destroy(ContactListAppContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
---
+++ Tests/ContactListTests.cs
@@ -0,0 +1,85 @@
+﻿using ContactList.DataContexts;
+using ContactList.DataServices;
+using ContactList.Models.Request;
+using Microsoft.EntityFrameworkCore;
+using Tests;
+
+namespace TestProject1.Authentification
+{
+    public class ContactListTests : IDisposable
+    {
+        protected readonly ContactListAppContext Context;
+        public ContactListTests()
+        {
+            Context  = ContactListContextFactory.Create();
+        }
+        public void Dispose()
+        {
+            ContactListContextFactory.Destroy(Context);
+        }
+
+        [Fact]
+        public async Task SignUpAndSingInUserCommandHandler_Success()
+        {
+            // Arrange
+            var handler = new UserService(Context);
+            var userName = "Test";
+            var password = "Test"; ;
+            var email = "test@example.com";
+
+            // Act
+            var user = handler.CreateAndAdd(userName, password, userName, email);
+
+            // Assert
+            Assert.NotNull(handler.GetUser(user.Login));
+        }
+
+        [Fact]
+        public async Task CreateContactCommandHandler_Success()
+        {
+            // Arrange
+            var handler = new ContactsService(Context, ContactListContextFactory.UserId);
+            var requestData = new ContactRequest()
+            {
+                Name = "Contact For Create",
+                Email = "test@example.com",
+                phone_number = new string[] { "", "(123) 456-7890", "(321) 456-7890" },
+                phone_type = new string[] { "", "1", "2" }
+            };
+
+            // Act
+            var contact = handler.Create(requestData);
+
+            // Assert
+            Assert.NotNull(
+                await Context.Contacts.SingleOrDefaultAsync(c =>
+                    c.Id == contact.Id && c.Email == requestData.Email &&
+                    c.Name == requestData.Name
+                ));
+        }
+
+        //[Fact]
+        //public async Task EditContactCommandHandler_Success()
+        //{
+        //    // Arrange
+        //    var handler = new ContactsService(Context, ContactListContextFactory.UserId);
+        //    var requestData = new ContactRequest()
+        //    {
+        //        Name = "Contact For Create",
+        //        Email = "test@example.com",
+        //        phone_number = new string[] { "", "(123) 456-7890", "(321) 456-7890" },
+        //        phone_type = new string[] { "", "1", "2" }
+        //    };
+
+        //    // Act
+        //    var contact = handler.Create(requestData);
+
+        //    // Assert
+        //    Assert.NotNull(
+        //        await Context.Contacts.SingleOrDefaultAsync(c =>
+        //            c.Id == contact.Id && c.Email == requestData.Email &&
+        //            c.Name == requestData.Name
+        //        ));
+        //}
+    }
+}
